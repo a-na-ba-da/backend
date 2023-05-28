647e50e927ace63e3f9888245a738a8a276d7902
penekhun <penekhun@gmail.com>
2023-05-28 23:59:28 +0900

    I'll explain when you're older!

---
+++ src/main/java/kr/anabada/anabadaserver/AnabadaServerApplication.java
@@ -0,0 +1,13 @@
+package kr.anabada.anabadaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnabadaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnabadaServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/anabada/anabadaserver/AnabadaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.anabada.anabadaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnabadaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
