13e5a22663563488c4b7de67402a8d2fb462416f
penekhun <penekhun@gmail.com>
2023-06-11 01:23:42 +0900

    refactor: 파일 이름 변경
    보다 이해하기 쉬운 이름으로 변경하였음.

--- src/main/java/kr/anabada/anabadaserver/common/dto/DomainType.java
+++ src/main/java/kr/anabada/anabadaserver/common/dto/DomainType.java
@@ -1,19 +1,19 @@
 package kr.anabada.anabadaserver.common.dto;
 
-public enum ImageType {
+public enum DomainType {
     BUY_TOGETHER("buy-together", "같이 사요"),
     KNOW_TOGETHER("know-together", "같이알아요");
 
     private final String typeStr;
     private final String name;
 
-    ImageType(String domain, String name) {
+    DomainType(String domain, String name) {
         this.typeStr = domain;
         this.name = name;
     }
 
-    public static ImageType of(String domain) {
-        for (ImageType imageType : values()) {
+    public static DomainType of(String domain) {
+        for (DomainType imageType : values()) {
             if (imageType.typeStr.equals(domain)) {
                 return imageType;
             }
