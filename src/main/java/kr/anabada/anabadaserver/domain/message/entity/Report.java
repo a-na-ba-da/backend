b561ce4ddca94263b901bde1d8f2369a36260f87
penekhun <penekhun@gmail.com>
2023-06-11 21:25:09 +0900

    fix: 요구사항에 따른 신고 테이블 칼럼 추가
    원래는 메세지로만 신고를 하려고 해서, 신고타입을 염두하지 않았는데, 생각해보니 일반 포스팅 신고 등등도 가능해야 할듯하여 신고타입과 신고타입 테이블 pk 을 추가하였음.
    
    추가로 신고 처리 여부도 필요할까 싶어서 넣었음.

--- src/main/java/kr/anabada/anabadaserver/domain/message/entity/Report.java
+++ src/main/java/kr/anabada/anabadaserver/domain/message/entity/Report.java
@@ -1,29 +1,55 @@
 package kr.anabada.anabadaserver.domain.message.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "report")
+@EntityListeners(AuditingEntityListener.class)
 public class Report {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id", nullable = false)
     private Long id;
 
+    @Comment("신고 타입")
+    @Column(name = "report_type", nullable = false, length = 15)
+    private String reportType;
+
+    @Comment("신고 대상 PK (타입 테이블 내 index)")
+    @Column(name = "report_target_id", nullable = false)
+    private Long reportTargetId;
+
+    @Comment("신고자 ID")
     @Column(name = "reporter_user_id", nullable = false)
     private Long reporterUserId;
 
+    @Comment("신고 대상 ID")
     @Column(name = "reported_user_id", nullable = false)
     private Long reportedUserId;
 
+    @Comment("신고 내용")
     @Column(name = "content", length = 500)
     private String content;
 
+    @Builder.Default
+    @Comment("신고 처리 여부")
+    @Column(name = "confirmed")
+    private Boolean confirmed = false;
+
+    @CreatedDate
     @Column(name = "created_at", nullable = false)
     private LocalDateTime createdAt;
-
 }
