5a95c38474bb4cb2a44a61bcb58b35e00c7e3add
penekhun <penekhun@gmail.com>
2023-06-11 16:57:41 +0900

    feat: Exception Response 규격 통일화
    MethodArgumentNotValidException(유효성 검증 실패시 throw) 과 IllegalArgumentException에 대해서 기존 공통 API Response 규격을 따르도록 하였습니다.

--- src/main/java/kr/anabada/anabadaserver/global/exception/GlobalExceptionHandler.java
+++ src/main/java/kr/anabada/anabadaserver/global/exception/GlobalExceptionHandler.java
@@ -1,15 +1,43 @@
 package kr.anabada.anabadaserver.global.exception;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Objects;
+
+import static kr.anabada.anabadaserver.global.exception.ErrorCode.ILLEGAL_ARGUMENT_EXCEPTION;
+import static kr.anabada.anabadaserver.global.exception.ErrorCode.INVALID_INPUT_VALUE;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+
+    /**
+     * jakarta.validation.constraints.* 유효성 검사 실패 시, Response 규격에 맞춰 에러 메시지를 반환한다.
+     */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        final ErrorResponse response = new ErrorResponse(INVALID_INPUT_VALUE.getErrorCode(), INVALID_INPUT_VALUE.getMessage(), Objects.requireNonNull(e.getBindingResult().getFieldError()).getDefaultMessage());
+        return new ResponseEntity<>(response, BAD_REQUEST);
+    }
+
+    /**
+     * IllegalArgumentException 발생 시, Response 규격에 맞춰 에러 메시지를 반환한다.
+     */
+    @ExceptionHandler(value = {IllegalArgumentException.class})
+    protected ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException e) {
+        final ErrorResponse response = new ErrorResponse(ILLEGAL_ARGUMENT_EXCEPTION.getErrorCode(), ILLEGAL_ARGUMENT_EXCEPTION.getMessage(), e.getMessage());
+        return new ResponseEntity<>(response, BAD_REQUEST);
+    }
+
+    /**
+     * CustomException 발생 시, Response 규격에 맞춰 에러 메시지를 반환한다.
+     */
     @ExceptionHandler(value = {CustomException.class})
     protected ResponseEntity<ErrorResponse> handleCustomException(CustomException e) {
         return ErrorResponse.toResponseEntity(e.getErrorCode());
     }
-
 }
