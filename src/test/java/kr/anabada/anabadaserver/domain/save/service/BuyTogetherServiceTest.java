1e74b9e3627aad9278305875f81b9b2d929ec382
penekhun <penekhun@gmail.com>
2023-06-11 14:03:57 +0900

    feat: 아껴쓰기 - 같이사요 입력값 유효성 검증 로직 테스트 추가

--- src/test/java/kr/anabada/anabadaserver/domain/save/service/BuyTogetherServiceTest.java
+++ src/test/java/kr/anabada/anabadaserver/domain/save/service/BuyTogetherServiceTest.java
@@ -5,6 +5,8 @@ import kr.anabada.anabadaserver.domain.save.dto.BuyTogetherDto;
 import kr.anabada.anabadaserver.domain.save.entity.Save;
 import kr.anabada.anabadaserver.domain.save.repository.SaveRepository;
 import kr.anabada.anabadaserver.domain.user.entity.User;
+import kr.anabada.anabadaserver.global.exception.CustomException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -16,6 +18,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Collections;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -54,6 +57,7 @@ class BuyTogetherServiceTest {
                 .productUrl("https://www.naver.com")
                 .buyDate(LocalDate.now())
                 .createdAt(LocalDateTime.now())
+                .images(Collections.singletonList("image"))
                 .build();
         Save buyTogether = buyTogetherDto.toEntity();
         ReflectionTestUtils.setField(buyTogether, "id", fakePostId);
@@ -68,6 +72,92 @@ class BuyTogetherServiceTest {
         // Then
         Save findPost = saveRepository.findById(result.getId()).get();
         assertEquals(buyTogetherDto.getTitle(), findPost.getTitle());
+    }
+
+    @Test
+    @Transactional
+    @DisplayName("아껴쓰기 - 같이사요 | 게시물 실패 테스트 | 비대면 전달인데, 위치정보가 없는 경우")
+    void 게시물_등록_실패1() {
+        // Given
+        User user = User.builder()
+                .id(1L)
+                .email("test@test.com")
+                .role("ROLE_USER")
+                .activated(true)
+                .nickname("testtest")
+                .build();
+        BuyTogetherDto buyTogetherDto = BuyTogetherDto.builder()
+                .title("title")
+                .content("content")
+                .pay(10000)
+                .isOnlineDelivery(false)
+                .buyPlaceLat(null)
+                .buyPlaceLng(null)
+                .images(Collections.singletonList("image"))
+                .buyDate(LocalDate.now())
+                .createdAt(LocalDateTime.now())
+                .build();
+        // When & Then
+        Assertions.assertThrows(CustomException.class, () -> {
+            buyTogetherService.createNewBuyTogetherPost(user, buyTogetherDto);
+        });
+    }
+
+    @Test
+    @Transactional
+    @DisplayName("아껴쓰기 - 같이사요 | 게시물 실패 테스트 | 이미지가 한장도 없을 때")
+    void 게시물_등록_실패2() {
+        // Given
+        User user = User.builder()
+                .id(1L)
+                .email("test@test.com")
+                .role("ROLE_USER")
+                .activated(true)
+                .nickname("testtest")
+                .build();
+        BuyTogetherDto buyTogetherDto = BuyTogetherDto.builder()
+                .title("title")
+                .content("content")
+                .pay(10000)
+                .isOnlineDelivery(false)
+                .buyPlaceLat(null)
+                .buyPlaceLng(null)
+                .buyDate(LocalDate.now())
+                .createdAt(LocalDateTime.now())
+                .build();
+        // When & Then
+        Assertions.assertThrows(CustomException.class, () -> {
+            buyTogetherService.createNewBuyTogetherPost(user, buyTogetherDto);
+        });
+    }
+
+    @Test
+    @Transactional
+    @DisplayName("아껴쓰기 - 같이사요 | 게시물 실패 테스트 | 구매 예정일이 과거인 경우")
+    void 게시물_등록_실패3() {
+        // Given
+        User user = User.builder()
+                .id(1L)
+                .email("test@test.com")
+                .role("ROLE_USER")
+                .activated(true)
+                .nickname("testtest")
+                .build();
+        BuyTogetherDto buyTogetherDto = BuyTogetherDto.builder()
+                .title("title")
+                .content("content")
+                .pay(10000)
+                .isOnlineDelivery(false)
+                .buyPlaceLat(null)
+                .buyPlaceLng(null)
+                .images(Collections.singletonList("image"))
+                .buyDate(LocalDate.now().minusDays(1))
+                .createdAt(LocalDateTime.now())
+                .build();
+        // When & Then
+        Assertions.assertThrows(CustomException.class, () -> {
+            buyTogetherService.createNewBuyTogetherPost(user, buyTogetherDto);
+        });
     }
 
 }
