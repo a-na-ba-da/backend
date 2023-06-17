e2d5ce26cf0fcdcec86a7fb07d6ed3680576ba72
penekhun <penekhun@gmail.com>
2023-06-18 00:37:43 +0900

    docs: API 문서화

--- src/main/java/kr/anabada/anabadaserver/common/controller/ImageUploadController.java
+++ src/main/java/kr/anabada/anabadaserver/common/controller/ImageUploadController.java
@@ -1,5 +1,9 @@
 package kr.anabada.anabadaserver.common.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import kr.anabada.anabadaserver.common.dto.DomainType;
 import kr.anabada.anabadaserver.common.service.ImageUploadService;
 import kr.anabada.anabadaserver.domain.user.entity.User;
@@ -15,14 +19,23 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/image")
+@Tag(name = "common", description = "각 도메인에서 공통으로 사용되는 API")
 public class ImageUploadController {
     private final ImageUploadService imageUploadService;
 
     @PostMapping("")
-    public List<String> uploadImage(@CurrentUser User user, MultipartFile[] uploadFile, @RequestParam String type) {
+    @Operation(summary = "이미지 업로드", description = "한개 혹은 여러개의 이미지를 업로드합니다. 성공하면 이미지 파일 이름들을 Array 형태로 반환합니다.")
+    @ApiResponse(responseCode = "200", description = "이미지 업로드 성공")
+    public List<String> uploadImage(@CurrentUser User user,
+                                    @Schema(description = "업로드할 이미지 파일들", requiredMode = REQUIRED)
+                                    MultipartFile[] uploadFile,
+                                    @RequestParam @Schema(description = "이미지 타입", example = "BUY_TOGETHER, KNOW_TOGETHER ....")
+                                    String type) {
         if (user == null) {
             throw new CustomException(ErrorCode.ONLY_ACCESS_USER);
         }
--- src/main/java/kr/anabada/anabadaserver/domain/report/dto/ReportDto.java
+++ src/main/java/kr/anabada/anabadaserver/domain/report/dto/ReportDto.java
@@ -1,5 +1,6 @@
 package kr.anabada.anabadaserver.domain.report.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import kr.anabada.anabadaserver.common.dto.DomainType;
 import lombok.AllArgsConstructor;
@@ -10,10 +11,13 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 public class ReportDto {
+    @Schema(description = "신고 타입", example = "BUY_TOGETHER")
     @NotNull(message = "신고 타입을 입력해주세요")
     DomainType type;
+    @Schema(description = "신고 대상 포스트 혹은 메세지의 id", example = "1")
     @NotNull(message = "신고 대상을 입력해주세요")
     Long postId;
+    @Schema(description = "신고 내용", example = "이 사람이 불쾌해요...")
     @NotNull(message = "신고 내용을 입력해주세요")
     String message;
 }
