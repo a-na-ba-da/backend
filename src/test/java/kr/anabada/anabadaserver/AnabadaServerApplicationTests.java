52c09a71bc3b92b81fc921495c84bbf5f2329267
penekhun <penekhun@gmail.com>
2023-06-20 03:21:54 +0900

    test: 사용하지 않는 test 비활성화

--- src/test/java/kr/anabada/anabadaserver/AnabadaServerApplicationTests.java
+++ src/test/java/kr/anabada/anabadaserver/AnabadaServerApplicationTests.java
@@ -1,5 +1,6 @@
 package kr.anabada.anabadaserver;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class AnabadaServerApplicationTests {
 
     @Test
+    @Disabled
     void contextLoads() {
     }
 
