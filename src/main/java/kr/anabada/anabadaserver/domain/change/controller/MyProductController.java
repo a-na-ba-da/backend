fabe0d458dbb31a2da6d062c23468c97358534e1
penekhun <penekhun@gmail.com>
2023-06-20 23:49:41 +0900

    docs: 내 상품 API 문서화 추가

--- src/main/java/kr/anabada/anabadaserver/domain/change/controller/MyProductController.java
+++ src/main/java/kr/anabada/anabadaserver/domain/change/controller/MyProductController.java
@@ -1,5 +1,7 @@
 package kr.anabada.anabadaserver.domain.change.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import kr.anabada.anabadaserver.domain.change.dto.MyProductRequest;
 import kr.anabada.anabadaserver.domain.change.dto.MyProductResponse;
@@ -19,11 +21,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/changing/my-product")
+@RequestMapping("/api/v1/changing")
+@Tag(name = "바꿔쓰기", description = "바꿔쓰기 관련 API")
 public class MyProductController {
     private final MyProductService myProductService;
 
-    @PostMapping("")
+    @Operation(summary = "바꿔쓰기에 사용할 내 상품 등록")
     public void createMyProduct(@CurrentUser User user, @Valid MyProductRequest request) {
         if (user == null)
             throw new CustomException(ErrorCode.ONLY_ACCESS_USER);
@@ -31,7 +34,7 @@ public class MyProductController {
         myProductService.createMyProduct(user, request);
     }
 
-    @GetMapping("")
+    @Operation(summary = "내 상품 목록 조회")
     public Page<MyProductResponse> getMyProduct(@CurrentUser User user, String searchWord, Pageable pageable) {
         if (user == null)
             throw new CustomException(ErrorCode.ONLY_ACCESS_USER);
