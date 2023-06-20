9b62e050de1c29c01f522689a88cbbb6570674ea
penekhun <penekhun@gmail.com>
2023-06-21 00:32:04 +0900

    docs: 상품 응답 객체 설명 추가

--- src/main/java/kr/anabada/anabadaserver/domain/change/dto/MyProductResponse.java
+++ src/main/java/kr/anabada/anabadaserver/domain/change/dto/MyProductResponse.java
@@ -11,11 +11,24 @@ import java.util.UUID;
 @NoArgsConstructor
 @Schema(description = "상품 조회 응답")
 public class MyProductResponse {
+    @Schema(description = "상품 id", example = "1")
     private Long id;
+
+    @Schema(description = "상품 소유자 정보")
     private UserDto owner;
+
+    @Schema(description = "상품 이름", example = "이스트라 AN753UHD 스마트 THE 울트라")
     private String name;
+
+    @Schema(description = "상품 설명", example = "상품 설명입니다.")
     private String content;
+
+    @Schema(description = "상품 가격", example = "10000")
     private int originalPrice;
+
+    @Schema(description = "상품 상태", example = "AVAILABLE")
     private ProductStatus status;
+
+    @Schema(description = "상품 대표 이미지", example = "6d4ca57f-71c6-4886-b464-6d2847b3ebb8")
     private UUID image;
 }
