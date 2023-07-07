3dfd1060c3db9625702fa6c34cf8eff511374565
penekhun <penekhun@gmail.com>
2023-07-07 12:56:14 +0900

    fix: 교환 요청 생성 로직 수정
    ChangeRequest 연관관계 매핑등에 따라서
    로직을 수정하였습니다.

--- src/main/java/kr/anabada/anabadaserver/domain/change/service/ProductChangeService.java
+++ src/main/java/kr/anabada/anabadaserver/domain/change/service/ProductChangeService.java
@@ -1,5 +1,6 @@
 package kr.anabada.anabadaserver.domain.change.service;
 
+import kr.anabada.anabadaserver.domain.change.dto.ChangeRequestResponse;
 import kr.anabada.anabadaserver.domain.change.dto.ChangeRequestStatus;
 import kr.anabada.anabadaserver.domain.change.dto.ProductStatus;
 import kr.anabada.anabadaserver.domain.change.entity.ChangeRequest;
@@ -14,7 +15,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -31,17 +33,18 @@ public class ProductChangeService {
     }
 
     @Transactional
-    public void changeRequest(User user, long targetProductId, List<Long> toChangeProductIds, String message) {
+    public void changeRequest(User requestUser, long targetProductId, List<Long> toChangeProductIds, String message) {
         if (toChangeProductIds == null || toChangeProductIds.isEmpty())
             throw new IllegalArgumentException("교환 신청할 물건은 최소 1개 이상이어야 합니다.");
 
-        List<MyProduct> myProducts = productRepository.findAllByOwnerAndStatus(user, ProductStatus.AVAILABLE);
-
         // check toChange products are available and in my products
-        for (Long toChangeProductId : toChangeProductIds) {
-            if (myProducts.stream().noneMatch(myProduct -> Objects.equals(myProduct.getId(), toChangeProductId)))
-                throw new IllegalArgumentException(String.format("%d는 변경 신청 가능한 물건이 아닙니다.", toChangeProductId));
-        }
+        Set<MyProduct> myProductsToChange = productRepository.findAllByOwnerAndStatus(requestUser, ProductStatus.AVAILABLE)
+                .stream()
+                .filter(product -> toChangeProductIds.contains(product.getId()))
+                .collect(Collectors.toSet());
+
+        if (myProductsToChange.size() != toChangeProductIds.size())
+            throw new IllegalArgumentException("교환 신청에 사용된 나의 물건 중 잘못된 물건이 포함되어 있습니다.");
 
         // check target product is available
         MyProduct targetProduct = productRepository.findById(targetProductId).orElseThrow(() -> new IllegalArgumentException("상대 물건이 존재하지 않습니다."));
@@ -53,18 +56,19 @@ public class ProductChangeService {
 
         // create change request
         ChangeRequest request = ChangeRequest.builder()
-                .productId(targetProductId)
+                .targetProduct(targetProduct)
                 .status(ChangeRequestStatus.REQUESTING)
                 .message(message)
-                .requesterId(user.getId())
+                .requester(requestUser)
+                .requestee(targetProduct.getOwner())
                 .build();
         changeRequestRepository.save(request);
 
         // create change request detail
-        List<ChangeRequestProduct> changeProducts = toChangeProductIds.stream()
-                .map(id -> {
+        List<ChangeRequestProduct> changeProducts = myProductsToChange.stream()
+                .map(myProduct -> {
                     ChangeRequestProduct changeRequestProduct = ChangeRequestProduct.builder()
-                            .productId(id)
+                            .product(myProduct)
                             .build();
                     changeRequestProduct.setChangeRequest(request);
                     return changeRequestProduct;
