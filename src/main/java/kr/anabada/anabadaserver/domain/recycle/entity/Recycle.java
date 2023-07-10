efcc1cd849a12687bb012f1a9d424dfd452a77f4
PENEKhun <penekhun@gmail.com>
2023-07-10 18:50:50 +0900

    fix: Recycle 엔티티 BaseTimeEntity 상속

--- src/main/java/kr/anabada/anabadaserver/domain/recycle/entity/Recycle.java
+++ src/main/java/kr/anabada/anabadaserver/domain/recycle/entity/Recycle.java
@@ -1,7 +1,7 @@
 package kr.anabada.anabadaserver.domain.recycle.entity;
 
 import jakarta.persistence.*;
-import kr.anabada.anabadaserver.domain.recycle.dto.RecyclePostRequestDto;
+import kr.anabada.anabadaserver.common.entity.BaseTimeEntity;
 import kr.anabada.anabadaserver.domain.recycle.dto.RecyclePostResponseDto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -9,12 +9,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.util.StringUtils;
-
-import java.time.LocalDateTime;
 
 @Getter
 @Entity
@@ -23,9 +18,8 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Table(name = "recycle")
 @Where(clause = "is_removed = false")
-@EntityListeners(AuditingEntityListener.class)
 @SQLDelete(sql = "UPDATE recycle SET is_removed = true WHERE id = ?")
-public class Recycle {
+public class Recycle extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
@@ -40,19 +34,10 @@ public class Recycle {
     @Column(name = "content", length = 300)
     private String content;
 
-    @CreatedDate
-    @Column(name = "created_at", nullable = false)
-    private LocalDateTime createdAt;
-
-    @LastModifiedDate
-    @Column(name = "modified_at")
-    private LocalDateTime modifiedAt;
-
     @Builder.Default
     @Column(name = "is_removed", nullable = false)
     private Boolean isRemoved = false;
-
-
+    
     public RecyclePostResponseDto toDto() {
         return RecyclePostResponseDto.builder()
                 .title(getTitle())
@@ -60,7 +45,7 @@ public class Recycle {
                 .build();
     }
 
-    public void editPost(String title, String content){
+    public void editPost(String title, String content) {
         if (StringUtils.hasText(title))
             this.title = title;
 
