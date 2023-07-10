08814eac28c733a701203a61b889e67e395f8e55
penekhun <penekhun@gmail.com>
2023-07-10 20:23:57 +0900

    docs: 아껴쓰기 검색 API 문서 내용 구체화

--- src/main/java/kr/anabada/anabadaserver/domain/save/dto/request/SaveSearchRequestDto.java
+++ src/main/java/kr/anabada/anabadaserver/domain/save/dto/request/SaveSearchRequestDto.java
@@ -1,16 +1,20 @@
 package kr.anabada.anabadaserver.domain.save.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
-@Setter
 @NoArgsConstructor
+@Schema(description = "아껴쓰기 검색 요청")
 public class SaveSearchRequestDto {
+    @Schema(description = "온라인 구매만 검색 여부", example = "true")
     private Boolean onlyOnlineBought;
+    @Schema(description = "나의 위도", example = "37.123456")
     private Double lat;
+    @Schema(description = "나의 경도", example = "127.123456")
     private Double lng;
+    @Schema(description = "검색 반경 (meter)", example = "1000")
     private Double distance;
 
     public SaveSearchRequestDto(boolean onlyOnlineBought, Double lat, Double lng, Double distance) {
