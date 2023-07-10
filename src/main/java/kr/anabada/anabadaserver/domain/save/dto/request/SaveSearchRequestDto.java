4f12236a95f11f2e2d4b90f8680527680275d2c7
penekhun <penekhun@gmail.com>
2023-07-10 20:24:45 +0900

    refactor: 아껴쓰기 검색 DTO 패키지 변경

--- src/main/java/kr/anabada/anabadaserver/domain/save/dto/request/SaveSearchRequestDto.java
+++ src/main/java/kr/anabada/anabadaserver/domain/save/dto/request/SaveSearchRequestDto.java
@@ -1,4 +1,4 @@
-package kr.anabada.anabadaserver.domain.save.dto;
+package kr.anabada.anabadaserver.domain.save.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
