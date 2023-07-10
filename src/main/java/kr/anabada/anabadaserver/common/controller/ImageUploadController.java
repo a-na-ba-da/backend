076e90aef6b2b680ff2702a4420740b49fe013a9
penekhun <penekhun@gmail.com>
2023-07-10 19:59:04 +0900

    docs: 이미지 API 문서 내용 구체화

--- src/main/java/kr/anabada/anabadaserver/common/controller/ImageUploadController.java
+++ src/main/java/kr/anabada/anabadaserver/common/controller/ImageUploadController.java
@@ -1,7 +1,8 @@
 package kr.anabada.anabadaserver.common.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import kr.anabada.anabadaserver.common.dto.DomainType;
@@ -17,8 +18,6 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
-import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
-
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/image")
@@ -27,12 +26,24 @@ public class ImageUploadController {
     private final ImageUploadService imageUploadService;
 
     @PostMapping("")
-    @Operation(summary = "이미지 업로드", description = "한개 혹은 여러개의 이미지를 업로드합니다. 성공하면 이미지 파일 이름들을 Array 형태로 반환합니다.")
-    @ApiResponse(responseCode = "200", description = "이미지 업로드 성공")
+    @Operation(
+            summary = "이미지 업로드",
+            description = "한개 혹은 여러개의 이미지를 업로드합니다. <br/>" +
+                    "업로드할땐 이미지파일 바이너리를 Array 형태로 요청합니다. 이미지가 하나인 경우엔 Array 형태로 보낼 필요 없습니다. <br/> <br/>" +
+                    "성공하면 이미지 파일 이름들을 Array 형태로 반환합니다."
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "이미지 업로드 성공"
+    )
     public GlobalResponse<List<String>> uploadImage(@CurrentUser User user,
-                                                    @Schema(description = "업로드할 이미지 파일들", requiredMode = REQUIRED)
+                                                    @Parameter(description = "업로드할 이미지 파일들 (blob)", required = true)
                                                     MultipartFile[] uploadFile,
-                                                    @RequestParam @Schema(description = "이미지 타입", example = "BUY_TOGETHER, KNOW_TOGETHER, MY_PRODUCT ....")
+                                                    @RequestParam @Parameter(description = "이미지 타입", examples = {
+                                                            @ExampleObject(name = "같이 사요", value = "BUY_TOGETHER", summary = "같이 사요 게시물에 사용되는 이미지"),
+                                                            @ExampleObject(name = "같이 알아요", value = "KNOW_TOGETHER", summary = "같이 알아요 게시물에 사용되는 이미지"),
+                                                            @ExampleObject(name = "물건(바꿔쓰기)", value = "MY_PRODUCT", summary = "바꿔쓰기에 사용되는 내 물건 이미지")
+                                                    })
                                                     String type) {
         if (user == null) {
             throw new CustomException(ErrorCode.ONLY_ACCESS_USER);
@@ -48,9 +59,20 @@ public class ImageUploadController {
 
 
     @GetMapping("/{fileName}")
-    @Operation(summary = "이미지 다운로드", description = "이미지 파일 이름을 받아서 이미지를 다운로드합니다.")
-    @ApiResponse(responseCode = "200", description = "이미지 다운로드 성공")
-    public byte[] downloadImage(@PathVariable String fileName) {
+    @Operation(
+            summary = "이미지 다운로드",
+            description = "이미지 파일 이름을 받아서 이미지를 다운로드합니다.<br/>" +
+                    "이미지 파일의 바이너리를 반환합니다." +
+                    "<br/><br/> 만약 썸네일용 이미지로 다운로드 하고 싶다면, 파일 이름의 맨앞(prefix)로 thumb 을 붙여서 요청하면 됩니다." +
+                    "<br/> 예시 : /api/v1/image/thumbnail_이미지파일이름"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "이미지 다운로드 성공"
+    )
+    public byte[] downloadImage(
+            @Parameter(description = "다운로드할 이미지 파일 이름<br/> 만약 썸네일 이미지로 조회하려면 prefix로 thumbnail_을 붙일 것", required = true)
+            @PathVariable String fileName) {
         return imageUploadService.downloadImage(fileName);
     }
 }
