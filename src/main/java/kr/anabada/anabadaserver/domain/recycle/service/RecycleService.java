48b97ed6f1cf3ed570ab6cd906b8d1bb35444ef8
penekhun <penekhun@gmail.com>
2023-07-22 20:46:43 +0900

    docs: 주석 todo 추가 (확인후 수정바람)

--- src/main/java/kr/anabada/anabadaserver/domain/recycle/service/RecycleService.java
+++ src/main/java/kr/anabada/anabadaserver/domain/recycle/service/RecycleService.java
@@ -3,7 +3,6 @@ package kr.anabada.anabadaserver.domain.recycle.service;
 import kr.anabada.anabadaserver.domain.recycle.dto.request.RecyclePostRequestDto;
 import kr.anabada.anabadaserver.domain.recycle.entity.Recycle;
 import kr.anabada.anabadaserver.domain.recycle.repository.RecycleRepository;
-import kr.anabada.anabadaserver.domain.save.entity.Save;
 import kr.anabada.anabadaserver.domain.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -17,7 +16,15 @@ public class RecycleService {
     private final RecycleRepository recycleRepository;
 
     @Transactional
-    public Recycle createNewRecyclePost(User writer, RecyclePostRequestDto recyclePostRequestDto){
+    public Recycle createNewRecyclePost(User writer, RecyclePostRequestDto recyclePostRequestDto) {
+        /*
+            TODO : 게시물에 대한 추가적인 validation 필요 - 성훈 2023.07.22.
+            예를 들면, 게시물의 제목이나 내용이 비어있는지, 혹은 글자수 제한이 있는지 등등
+
+            1. Service Layer 에서 단독으로 처리 한다.
+            2. Dto + Service Layer 에서 처리 한다. (권장)
+         */
+
         Recycle newRecyclePost = recyclePostRequestDto.toEntity(writer, recyclePostRequestDto);
 
         return recycleRepository.save(newRecyclePost);
