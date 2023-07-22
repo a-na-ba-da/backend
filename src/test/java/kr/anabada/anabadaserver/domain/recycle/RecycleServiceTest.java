200da3db02bc0e8163278b99ed1e1f54790512c7
hzim <hzim0422@gmail.com>
2023-07-22 19:41:40 +0900

    test: 다시쓰기 게시글 생성 테스트 개발

---
+++ src/test/java/kr/anabada/anabadaserver/domain/recycle/RecycleServiceTest.java
@@ -0,0 +1,52 @@
+package kr.anabada.anabadaserver.domain.recycle;
+
+import kr.anabada.anabadaserver.domain.recycle.dto.request.RecyclePostRequestDto;
+import kr.anabada.anabadaserver.domain.recycle.entity.Recycle;
+import kr.anabada.anabadaserver.domain.recycle.service.RecycleService;
+import kr.anabada.anabadaserver.domain.user.entity.User;
+import kr.anabada.anabadaserver.domain.user.repository.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@SpringBootTest
+public class RecycleServiceTest {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RecycleService recycleService;
+
+    @Test
+    @DisplayName("게시글 작성 성공")
+    void createNewRecyclePost(){
+        // given
+        User user = createUser("test@test.com", "testUser");
+        userRepository.save(user);
+
+        RecyclePostRequestDto recyclePostRequestDto = RecyclePostRequestDto.builder()
+                .title("title")
+                .content("content")
+                .build();
+
+        // when
+        Recycle newRecyclePost = recycleService.createNewRecyclePost(user, recyclePostRequestDto);
+
+        // then
+        Assertions.assertNotNull(newRecyclePost.getId());
+
+    }
+
+    private User createUser(String email, String nickname){
+        return User.builder()
+                .email(email)
+                .activated(true)
+                .nickname(nickname)
+                .role("ROLE_USER")
+                .build();
+    }
+}
