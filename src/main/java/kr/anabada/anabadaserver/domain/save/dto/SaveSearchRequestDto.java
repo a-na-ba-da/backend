3add2be3a7844ef7f21b162eac48ef2b24e5e1a9
penekhun <penekhun@gmail.com>
2023-07-30 16:26:45 +0900

    docs: 아껴쓰기 검색 요청값 문서화

--- src/main/java/kr/anabada/anabadaserver/domain/save/dto/SaveSearchRequestDto.java
+++ src/main/java/kr/anabada/anabadaserver/domain/save/dto/SaveSearchRequestDto.java
@@ -1,6 +1,7 @@
 package kr.anabada.anabadaserver.domain.save.dto;
 
 import io.swagger.v3.oas.annotations.Hidden;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,11 +10,17 @@ import org.springframework.util.StringUtils;
 @Getter
 @Setter
 @NoArgsConstructor
+@Schema(description = "검색 요청. 위치 기반 검색을 활성화 하려면, lat, lng, distance 필드를 모두 입력해주세요.")
 public class SaveSearchRequestDto {
+    @Schema(description = "온라인 구매만 검색하려면, true 로 설정해주세요.")
     private Boolean onlyOnlineBought;
+    @Schema(description = "위도")
     private Double lat;
+    @Schema(description = "경도")
     private Double lng;
+    @Schema(description = "검색 거리 (단위: m)")
     private Double distance;
+    @Schema(description = "검색 기능을 활성화 하려면, 해당 필드에 2자 이상의 검색어를 입력해주세요.")
     private String keyword;
 
     public SaveSearchRequestDto(boolean onlyOnlineBought, Double lat, Double lng, Double distance, String keyword) {
