811448b5c2640516424b4c43b6c5d96e34e343ac
hzim <hzim0422@gmail.com>
2023-08-13 17:01:21 +0900

    refactor: BaseTimeEntity 상속관계 추가 및 필요 어노테이션 추가

--- src/main/java/kr/anabada/anabadaserver/domain/recycle/entity/Recycle.java
+++ src/main/java/kr/anabada/anabadaserver/domain/recycle/entity/Recycle.java
@@ -1,29 +1,29 @@
 package kr.anabada.anabadaserver.domain.recycle.entity;
 
 import jakarta.persistence.*;
+import kr.anabada.anabadaserver.common.entity.BaseTimeEntity;
 import kr.anabada.anabadaserver.domain.recycle.dto.request.RecyclePostRequest;
 import kr.anabada.anabadaserver.domain.recycle.dto.response.RecycleResponse;
 import kr.anabada.anabadaserver.domain.user.entity.User;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.util.StringUtils;
 
-import java.time.LocalDateTime;
 
 @Getter
 @Entity
 @NoArgsConstructor
+@SuperBuilder
 @Table(name = "recycle")
 @Where(clause = "is_removed = false")
 @EntityListeners(AuditingEntityListener.class)
 @SQLDelete(sql = "UPDATE recycle SET is_removed = true WHERE id = ?")
-public class Recycle {
+public class Recycle extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
@@ -34,19 +34,18 @@ public class Recycle {
 
     @OneToOne
     @JoinColumn(name = "writer", nullable = false)
-//    @Column(name = "id", nullable = false)
     private User writer;
 
     @Column(name = "content", length = 300)
     private String content;
 
-    @CreatedDate
-    @Column(name = "created_at", nullable = false)
-    private LocalDateTime createdAt;
-
-    @LastModifiedDate
-    @Column(name = "modified_at")
-    private LocalDateTime modifiedAt;
+//    @CreatedDate
+//    @Column(name = "created_at", nullable = false)
+//    private LocalDateTime createdAt;
+//
+//    @LastModifiedDate
+//    @Column(name = "modified_at")
+//    private LocalDateTime modifiedAt;
 
     @Builder.Default
     @Column(name = "is_removed", nullable = false)
@@ -54,13 +53,11 @@ public class Recycle {
 
 
     @Builder
-    public Recycle(Long id, String title, User writer, String content, LocalDateTime createdAt, LocalDateTime modifiedAt, Boolean isRemoved) {
+    public Recycle(Long id, String title, User writer, String content, Boolean isRemoved) {
         this.id = id;
         this.title = title;
         this.writer = writer;
         this.content = content;
-        this.createdAt = createdAt;
-        this.modifiedAt = modifiedAt;
         this.isRemoved = isRemoved;
     }
 
