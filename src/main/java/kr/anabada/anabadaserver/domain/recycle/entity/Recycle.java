bd904bbb6e604e39cb60d005da948fc2f7149fe2
hzim <hzim0422@gmail.com>
2023-08-14 23:03:35 +0900

    refactor: Image 컬럼 추가

--- src/main/java/kr/anabada/anabadaserver/domain/recycle/entity/Recycle.java
+++ src/main/java/kr/anabada/anabadaserver/domain/recycle/entity/Recycle.java
@@ -2,6 +2,7 @@ package kr.anabada.anabadaserver.domain.recycle.entity;
 
 import jakarta.persistence.*;
 import kr.anabada.anabadaserver.common.entity.BaseTimeEntity;
+import kr.anabada.anabadaserver.common.entity.Image;
 import kr.anabada.anabadaserver.domain.recycle.dto.request.RecyclePostRequest;
 import kr.anabada.anabadaserver.domain.recycle.dto.response.RecycleResponse;
 import kr.anabada.anabadaserver.domain.user.entity.User;
@@ -9,10 +10,14 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.BatchSize;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.util.StringUtils;
+
+import java.util.List;
+import java.util.UUID;
 
 
 @Getter
@@ -51,14 +56,21 @@ public class Recycle extends BaseTimeEntity {
     @Column(name = "is_removed", nullable = false)
     private Boolean isRemoved = false;
 
+    @BatchSize(size = 100)
+    @JoinColumn(name = "post_id")
+    @OneToMany(fetch = FetchType.LAZY)
+    @Where(clause = "image_type = 'RECYCLE'")
+    private List<Image> images;
 
     @Builder
-    public Recycle(Long id, String title, User writer, String content, Boolean isRemoved) {
+    public Recycle(BaseTimeEntityBuilder<?, ?> b, Long id, String title, User writer, String content, Boolean isRemoved, List<Image> images) {
+        super(b);
         this.id = id;
         this.title = title;
         this.writer = writer;
         this.content = content;
         this.isRemoved = isRemoved;
+        this.images = images;
     }
 
     public void setWriter(User writer) {
@@ -78,6 +90,7 @@ public class Recycle extends BaseTimeEntity {
                 .title(getTitle())
                 .content(getContent())
                 .writer(getWriter().toDto())
+                .images(images.stream().map(Image::getId).map(UUID::toString).toList())
                 .build();
     }
 }
